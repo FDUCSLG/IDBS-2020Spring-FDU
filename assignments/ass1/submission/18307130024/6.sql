d558ee792999b7589ff0b8338cb24bedeb9de7f0
15711657500 <15711657500@163.com>
2020-03-14 17:52:25 +0800

    submission of 18307130024 for ass1

--- assignments/ass1/submission/18307130024/6.sql
+++ assignments/ass1/submission/18307130024/6.sql
@@ -1,8 +1,5 @@
-select distinct A.publisher
-from book as A, book as B, book as C
-where A.publisher = B.publisher
-    and B.publisher = C.publisher
-    and A.id != B.id
-    and B.id != C.id
-    and A.id != C.id
-order by A.publisher;
+select distinct publisher
+from book
+group by publisher
+having count(*) > 2
+order by publisher;
