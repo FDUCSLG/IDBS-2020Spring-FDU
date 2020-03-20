37819b41ceef40c0270119bc257a409414b62d02
XuYipei <1449097870@qq.com>
2020-03-20 11:59:33 +0800

    submission of 18307130103 for ass1

--- assignments/ass1/submission/18307130103/7.sql
+++ assignments/ass1/submission/18307130103/7.sql
@@ -2,4 +2,4 @@ select employee.name
 	from employee,record
 	where record.time>'2016-10-31' and record.employee_id=employee.id
 	group by employee.name
-	order by max(employee.id)
+	order by max(employee.id);
