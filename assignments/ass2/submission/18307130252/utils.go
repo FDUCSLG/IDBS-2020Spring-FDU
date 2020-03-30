75d83644936df31b6bcfdbe50e17efeae355b391
kleinercubs <1910481653@qq.com>
2020-03-30 23:51:54 +0800

    update from upstream, modify ass1

--- assignments/ass2/submission/18307130252/utils.go
+++ assignments/ass2/submission/18307130252/utils.go
@@ -6,6 +6,9 @@ import (
 
 	// YOUR CODE BEGIN remove the follow packages if you don't need them
 
+	"reflect"
+	"sync"
+
 	// YOUR CODE END
 
 	_ "github.com/go-sql-driver/mysql"
@@ -14,10 +17,10 @@ import (
 
 var (
 	// YOUR CODE BELOW
-	EvaluatorID   = "18307130252" // your student id, e.g. 18307130177
-	SubmissionDir = "../../../ass1/submission/18307130252" // the relative path the the submission directory of assignment 1, it should be "../../../ass1/submission/"
-	User          = "kleinercubs" // the user name to connect the database, e.g. root
-	Password      = "kleinercubs" // the password for the user name, e.g. xxx
+	EvaluatorID   = "18307130252"               // your student id, e.g. 18307130177
+	SubmissionDir = "../../../ass1/submission/" // the relative path the the submission directory of assignment 1, it should be "../../../ass1/submission/"
+	User          = "kleinercubs"               // the user name to connect the database, e.g. root
+	Password      = "kleinercubs"               // the password for the user name, e.g. xxx
 	// YOUR CODE END
 )
 
@@ -45,6 +48,64 @@ func ConcurrentCompareAndInsert(subs map[string]*Submission) {
 		fmt.Println("ConcurrentCompareAndInsert takes ", time.Since(start))
 	}()
 	// YOUR CODE BEGIN
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/ass1_result_evaluated_by_%s", User, Password, EvaluatorID))
+	if err != nil {
+		panic(nil)
+	}
+
+	type data struct {
+		sub  string
+		cmp  string
+		id   int
+		same int
+	}
+
+	bufferedChan := make(chan data, 128)
+	wg := sync.WaitGroup{}
+
+	for thread := 0; thread < 16; thread++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			//		for range subs {
+			//			for range subs {
+			//				for i := 0; i < NumSQL; i++ {
+			//					tmp := <-bufferedChan
+			for tmp := range bufferedChan {
+				s := fmt.Sprintf("INSERT INTO comparison_result VALUES ('%s', '%s', %d, %d)", tmp.sub, tmp.cmp, tmp.id, tmp.same)
+				//	fmt.Println("Receive %s %s %d %d", tmp.sub, tmp.cmp, tmp.id, tmp.same)
+				_, err := db.Exec(s)
+				if err != nil {
+					fmt.Println(s)
+					panic(err)
+				}
+				//				}
+				//			}
+			}
+		}()
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for submitter, sub := range subs {
+			for comparer, sub2 := range subs {
+				for i := 0; i < NumSQL; i++ {
+					var equal int
+					if reflect.DeepEqual(sub.sqlResults[i], sub2.sqlResults[i]) {
+						equal = 1
+					} else {
+						equal = 0
+					}
+					bufferedChan <- data{submitter, comparer, i + 1, equal}
+					//			fmt.Println("Send %s %s %d %d", submitter, comparer, i+1, equal)
+				}
+			}
+		}
+		close(bufferedChan)
+	}()
+
+	wg.Wait()
 
 	// YOUR CODE END
 }
@@ -55,13 +116,35 @@ func GetScoreSQL() string {
 	var SQL string
 	SQL = "SELECT 1" // ignore this line, it just makes the returned SQL a valid SQL if you haven't written yours.
 	// YOUR CODE BEGIN
-
+	SQL = `INSERT INTO score
+	WITH RESULT AS(
+		SELECT submitter, item, count(is_equal) AS vote
+		FROM comparison_result
+		WHERE is_equal = 1
+		GROUP BY submitter, item
+	)
+	SELECT submitter, item, IF (vote IN (SELECT MAX(Tmp.vote) FROM RESULT AS Tmp WHERE Tmp.item = RESULT.item), 1, 0),vote
+	FROM RESULT`
 	// YOUR CODE END
 	return SQL
 }
 
 func GetScore(db *sql.DB, subs map[string]*Submission) {
 	// YOUR CODE BEGIN
+
+	rows, err := db.Query("SELECT * FROM score")
+
+	if err != nil {
+		panic(err)
+	}
+
+	var submitter string
+	var item, score, vote int
+
+	for rows.Next() {
+		rows.Scan(&submitter, &item, &score, &vote)
+		subs[submitter].score[item] = score
+	}
 
 	// YOUR CODE END
 }
