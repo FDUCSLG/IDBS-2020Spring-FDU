cf0ae90065cd3419032ff5bf5b4dd57ae4e323e8
unicornt <unicorntwin@outlook.com>
2020-04-02 01:31:39 +0800

    submission of 18307130104 for ass2

--- assignments/ass2/submission/18307130104/utils.go
+++ assignments/ass2/submission/18307130104/utils.go
@@ -5,7 +5,8 @@ import (
 	"time"
 
 	// YOUR CODE BEGIN remove the follow packages if you don't need them
-
+	"reflect"
+	"sync"
 	// YOUR CODE END
 
 	_ "github.com/go-sql-driver/mysql"
@@ -14,10 +15,10 @@ import (
 
 var (
 	// YOUR CODE BELOW
-	EvaluatorID   = "" // your student id, e.g. 18307130177
-	SubmissionDir = "" // the relative path the the submission directory of assignment 1, it should be "../../../ass1/submission/"
-	User          = "" // the user name to connect the database, e.g. root
-	Password      = "" // the password for the user name, e.g. xxx
+	EvaluatorID   = "18307130104" // your student id, e.g. 18307130177
+	SubmissionDir = "../../../ass1/submission/" // the relative path the the submission directory of assignment 1, it should be "../../../ass1/submission/"
+	User          = "unicornt" // the user name to connect the database, e.g. root
+	Password      = "twinblade" // the password for the user name, e.g. xxx
 	// YOUR CODE END
 )
 
@@ -45,7 +46,72 @@ func ConcurrentCompareAndInsert(subs map[string]*Submission) {
 		fmt.Println("ConcurrentCompareAndInsert takes ", time.Since(start))
 	}()
 	// YOUR CODE BEGIN
-
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/ass1_result_evaluated_by_%s", User, Password, EvaluatorID))
+	if err != nil {
+		panic(err)
+	}
+	type safedb struct{
+		db *sql.DB
+		mux sync.Mutex
+	}
+	const tot = 30
+	var dbarr[tot] safedb
+	for i := 0; i < tot; i++ {
+		dbarr[i] = safedb{ db: db }
+	}
+	var wg sync.WaitGroup
+	cur := 0
+	for submitter, sub := range subs {
+		for comparer, sub2 := range subs {
+			for i := 0; i < NumSQL; i++ {
+				var equal int
+				if reflect.DeepEqual(sub.sqlResults[i], sub2.sqlResults[i]) {
+					equal = 1
+				} else {
+					equal = 0
+				}
+				wg.Add(1)
+				cur ++
+				if cur == tot {
+					cur = 0
+				}
+				go func(wg *sync.WaitGroup, cur1 int, submitter string, comparer string, equal int, i int){
+					dbarr[cur1].mux.Lock()
+					defer dbarr[cur1].mux.Unlock()
+					s := fmt.Sprintf("INSERT INTO comparison_result VALUES ('%s', '%s', %d, %d)", submitter, comparer, i+1, equal)
+					_, err := dbarr[cur1].db.Exec(s)
+					if err != nil {
+						fmt.Println(s)
+						panic(err)
+					}
+					wg.Done()
+				}(&wg, cur, submitter, comparer, equal, i)
+			}
+		}
+		wg.Wait()
+	}
+	/*db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/ass1_result_evaluated_by_%s", User, Password, EvaluatorID))
+	if err != nil {
+		panic(err)
+	}
+	for submitter, sub := range subs {
+		for comparer, sub2 := range subs {
+			for i := 0; i < NumSQL; i++ {
+				var equal int
+				if reflect.DeepEqual(sub.sqlResults[i], sub2.sqlResults[i]) {
+					equal = 1
+				} else {
+					equal = 0
+				}
+				s := fmt.Sprintf("INSERT INTO comparison_result VALUES ('%s', '%s', %d, %d)", submitter, comparer, i+1, equal)
+				_, err := db.Exec(s)
+				if err != nil {
+					fmt.Println(s)
+					panic(err)
+				}
+			}
+		}
+	}*/
 	// YOUR CODE END
 }
 
@@ -55,13 +121,36 @@ func GetScoreSQL() string {
 	var SQL string
 	SQL = "SELECT 1" // ignore this line, it just makes the returned SQL a valid SQL if you haven't written yours.
 	// YOUR CODE BEGIN
-
+	SQL = "INSERT INTO score (submitter, item, score, vote) " +
+		  "(SELECT submitter, item, 1 AS score, vote " +
+		   "FROM (SELECT submitter, item, COUNT(*) AS vote FROM comparison_result WHERE is_equal = 1 GROUP BY submitter, item) AS X " +
+		   "WHERE vote >= ALL(SELECT COUNT(*) " +
+		   					 "FROM comparison_result AS Y " +
+		   					 "WHERE Y.is_equal = 1 AND X.item = Y.item " +
+		   					 "GROUP BY submitter)) " +
+		  "UNION " +
+		  "(SELECT submitter, item, 0 AS score, vote " +
+		   "FROM (SELECT submitter, item, COUNT(*) AS vote FROM comparison_result WHERE is_equal = 1 GROUP BY submitter, item) AS X " +
+		   "WHERE vote < SOME(SELECT COUNT(*) " +
+		   					 "FROM comparison_result AS Y " +
+		   					 "WHERE Y.is_equal = 1 AND X.item = Y.item " +
+		   					 "GROUP BY submitter)) " 
 	// YOUR CODE END
 	return SQL
 }
 
 func GetScore(db *sql.DB, subs map[string]*Submission) {
 	// YOUR CODE BEGIN
+	score, scoreerr := db.Query("SELECT submitter, item, score FROM score")
+	if scoreerr != nil {
+		panic(scoreerr)
+	}
 
+	for score.Next() {
+		var id string
+		var itid, sc int
+		score.Scan(&id, &itid, &sc)
+		subs[id].score[itid] = sc
+	}
 	// YOUR CODE END
 }
