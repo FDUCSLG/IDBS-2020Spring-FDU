3adbf54555e62b5b462e3be7c520b4bfd2d03b81
XuYipei <1449097870@qq.com>
2020-04-02 00:26:30 +0800

    Submission of Ass2 by 18307130103

--- assignments/ass2/submission/18307130103/utils.go
+++ assignments/ass2/submission/18307130103/utils.go
@@ -5,7 +5,8 @@ import (
 	"time"
 
 	// YOUR CODE BEGIN remove the follow packages if you don't need them
-
+	"reflect"
+	"sync"
 	// YOUR CODE END
 
 	_ "github.com/go-sql-driver/mysql"
@@ -14,10 +15,10 @@ import (
 
 var (
 	// YOUR CODE BELOW
-	EvaluatorID   = "" // your student id, e.g. 18307130177
-	SubmissionDir = "" // the relative path the the submission directory of assignment 1, it should be "../../../ass1/submission/"
-	User          = "" // the user name to connect the database, e.g. root
-	Password      = "" // the password for the user name, e.g. xxx
+	EvaluatorID   = "18307130103" // your student id, e.g. 18307130177
+	SubmissionDir = "../../../ass1/submission/" // the relative path the the submission directory of assignment 1, it should be "../../../ass1/submission/"
+	User          = "XuYipei" // the user name to connect the database, e.g. root
+	Password      = "123456" // the password for the user name, e.g. xxx
 	// YOUR CODE END
 )
 
@@ -45,7 +46,64 @@ func ConcurrentCompareAndInsert(subs map[string]*Submission) {
 		fmt.Println("ConcurrentCompareAndInsert takes ", time.Since(start))
 	}()
 	// YOUR CODE BEGIN
+/*	
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/ass1_result_evaluated_by_%s", User, Password, EvaluatorID))
+	if err != nil {
+		panic(err)
+	}
+	for submitter, sub := range subs {
+		for comparer, sub2 := range subs {
+			for i := 0; i < NumSQL; i++ {
+				var equal int
+				if reflect.DeepEqual(sub.sqlResults[i], sub2.sqlResults[i]) {
+					equal = 1
+				} else {
+					equal = 0
+				}
+				s := fmt.Sprintf("INSERT INTO comparison_result VALUES ('%s', '%s', %d, %d)", submitter, comparer, i+1, equal)
+				_, err := db.Exec(s)
+				if err != nil {
+					fmt.Println(s)
+					panic(err)
+				}
+			}
+		}
+	}
+*/
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/ass1_result_evaluated_by_%s", User, Password, EvaluatorID))
+	if err != nil {
+		panic(err)
+	}
 
+	wg := sync.WaitGroup{}
+	nm := make(chan string)
+	for submitter := range subs{	
+		go func(){
+			submitter := <- nm
+			sub := subs[submitter]
+			wg.Add(1)
+			for comparer, sub2 := range subs{
+				for i :=0; i < NumSQL; i++{
+					var equal int
+					if reflect.DeepEqual(sub.sqlResults[i], sub2.sqlResults[i]){
+						equal = 1
+					}else{
+						equal = 0
+					}
+					s := fmt.Sprintf("INSERT INTO comparison_result VALUES ('%s', '%s', %d, %d)", submitter, comparer, i+1, equal)
+					_, err := db.Exec(s)
+					if err != nil {
+						fmt.Println(s)
+						panic(err)
+					}
+				}
+			}
+			wg.Done()
+		}()
+		nm <- submitter
+	}
+	wg.Wait()
+	
 	// YOUR CODE END
 }
 
@@ -55,7 +113,7 @@ func GetScoreSQL() string {
 	var SQL string
 	SQL = "SELECT 1" // ignore this line, it just makes the returned SQL a valid SQL if you haven't written yours.
 	// YOUR CODE BEGIN
-
+	SQL = "INSERT INTO score(submitter, item, score, vote) SELECT final_table.submitter, final_table.item, final_table.score, final_table.vote FROM(WITH vote_table AS(SELECT submitter, item, SUM(is_equal) AS vote FROM comparison_result GROUP BY submitter, item), max_vote_table AS(SELECT item, MAX(vote) as mx FROM vote_table GROUP BY vote_table.item), merge_table AS(SELECT vote_table.submitter, vote_table.item, (vote_table.vote = max_vote_table.mx) AS score, vote_table.vote FROM vote_table INNER JOIN max_vote_table ON vote_table.item = max_vote_table.item) SELECT merge_table.submitter as submitter, merge_table.item as item, merge_table.score as score, merge_table.vote as vote FROM merge_table ORDER BY submitter ASC, item ASC)AS final_table;"
 	// YOUR CODE END
 	return SQL
 }
@@ -63,5 +121,17 @@ func GetScoreSQL() string {
 func GetScore(db *sql.DB, subs map[string]*Submission) {
 	// YOUR CODE BEGIN
 
+	rows, err := db.Query("SELECT * FROM score;")
+	if (err != nil){
+		panic(err)	
+	}			
+	for rows.Next(){
+		var str string
+		var item, score, vote int
+		rows.Scan(&str, &item, &score, &vote)
+//		fmt.Println(str, item, score,vote)
+		subs[str].score[item] = score
+	}
+	
 	// YOUR CODE END
 }
